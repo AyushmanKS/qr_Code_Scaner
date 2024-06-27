6d920e534a2eb083d9e485efdf2ffb9277e5f829
AyushmanKS <ayushmanks1845@gmail.com>
2024-06-27 17:11:51 +0530

    implement qr code scanner

--- lib/pages/scan_code.dart
+++ lib/pages/scan_code.dart
@@ -1,4 +1,8 @@
+import 'dart:developer';
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
 import 'package:qr_code_scanner/pages/generate_code.dart';
 
 class ScanCodePage extends StatefulWidget {
@@ -24,6 +28,34 @@ class _ScanCodePageState extends State<ScanCodePage> {
               },
               icon: const Icon(Icons.qr_code_scanner))
         ],
+      ),
+      body: MobileScanner(
+        controller: MobileScannerController(
+          detectionSpeed: DetectionSpeed.noDuplicates,
+          returnImage: true,
+        ),
+        onDetect: (capture) {
+          log('---------$capture---------');
+          List<Barcode> barcodes = capture.barcodes;
+          final Uint8List? image = capture.image;
+
+          for (final barcode in barcodes) {
+            log('-------------Barcode found!: ${barcode.rawValue}');
+          }
+          if (image != null) {
+            showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  title: Text(barcodes.first.rawValue ?? "No code found!"),
+                  content: Image(
+                    image: MemoryImage(image),
+                  ),
+                );
+              },
+            );
+          }
+        },
       ),
     );
   }
