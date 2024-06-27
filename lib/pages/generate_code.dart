3f6949213d3ed14ed7ebf22ae1d545056d167937
AyushmanKS <ayushmanks1845@gmail.com>
2024-06-27 12:40:31 +0530

    initial commit

---
+++ lib/pages/generate_code.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class GenerateCodePage extends StatefulWidget {
+  const GenerateCodePage({super.key});
+
+  @override
+  State<GenerateCodePage> createState() => _GenerateCodePageState();
+}
+
+class _GenerateCodePageState extends State<GenerateCodePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.qr_code))
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/pages/scan_code.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ScanCodePage extends StatefulWidget {
+  const ScanCodePage({super.key});
+
+  @override
+  State<ScanCodePage> createState() => _ScanCodePageState();
+}
+
+class _ScanCodePageState extends State<ScanCodePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.scanner))
+        ],
+      ),
+    );
+  }
+}
